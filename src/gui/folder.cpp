ac9d3bdfb3d3cca2c4213bf9852c530c93c0c38a
Christian Kamm <mail@ckamm.de>
2019-06-13 09:59:01 +0200

    FolderWatcher: Become unreliable if test notification fails #7241
    Necessary for some filesystems on windows that don't have full file
    watching capabilities.

--- src/gui/folder.cpp
+++ src/gui/folder.cpp
@@ -1043,6 +1043,7 @@ void Folder::registerFolderWatcher()
     connect(_folderWatcher.data(), &FolderWatcher::becameUnreliable,
         this, &Folder::slotWatcherUnreliable);
     _folderWatcher->init(path());
+    _folderWatcher->startNotificatonTest(path() + QLatin1String(".owncloudsync.log"));
 }
 
 void Folder::slotAboutToRemoveAllFiles(SyncFileItem::Direction dir, bool *cancel)
